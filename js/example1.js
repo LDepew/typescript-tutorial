464d561809d940ab2ebb6ccca6743d5586f733ba
Lance Depew <Metal_Death_Music@yahoo.com>
2021-03-29 14:40:56 -0400

    node example js

--- js/example1.js
+++ js/example1.js
@@ -1,4 +1,9 @@
 "use strict";
+var a = 0;
 var loaded = function () {
-    document.getElementById("header1").innerText = "Hello, Typescript is cool!";
+    //document.getElementById("header1").innerText = "Hello, Typescript is cool!";
 };
+
+
+
+console.log("this is a console.log displayed in node")
